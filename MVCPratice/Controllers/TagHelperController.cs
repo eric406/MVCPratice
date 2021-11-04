d9e3fcf9e908d14ff9a0ef6f9728554f60d6c526
EricHsu <ericeric4062@gmail.com>
2021-11-04 15:30:32 +0800

    feature: tag helper partial view

---
+++ MVCPratice/Controllers/TagHelperController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPratice.Models.Hero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPratice.Controllers
+{
+    public class TagHelperController : Controller
+    {
+        public List<Hero> heros { get; } = new List<Hero>
+        {
+             new Hero { Name = "Elon Musk", Brief="特斯拉創辦人 伊隆·馬斯克", Photo="ElonMusk.jpg", WikiUrl="https://goo.gl/46xeXx" },
+            new Hero { Name = "Mark Zuckerberg", Brief="Facebook創辦人 馬克·祖伯克", Photo="MarkZuckerberg.jpg", WikiUrl="https://goo.gl/BktGGA" },
+            new Hero { Name = "Steve Jobs", Brief="蘋果創辦人 史提夫·賈伯斯", Photo="SteveJobs.jpg", WikiUrl="https://goo.gl/nAiX0y" },
+            new Hero { Name = "Vader", Brief="帝國元帥  維達", Photo="Vader.jpg", WikiUrl="https://en.wikipedia.org/wiki/Darth_Vader" },
+            new Hero { Name = "Darth Mual", Brief="星際大戰 達斯摩", Photo="DarthMual.jpg", WikiUrl="https://goo.gl/5obLhX"},
+            new Hero { Name = "White Twilek", Brief="星際大戰 女絕地武士Twilek", Photo="WhiteTwilek.jpg", WikiUrl="https://goo.gl/reKzAu" },
+            new Hero { Name = "Obiwan", Brief="星際大戰 歐比旺Obiwan", Photo="Obiwan.jpg", WikiUrl="http://bit.ly/33gxdgt" },
+            new Hero { Name = "Merkel", Brief="德國總理 梅克爾", Photo="Merkel.jpg", WikiUrl="http://bit.ly/33huSlv" }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PartialTagHelper()
+        {
+            return View(heros);
+        }
+    }
+}
---
+++ MVCPratice/Models/Hero/Hero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPratice.Models.Hero
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brief { get; set; }
+        public string Photo { get; set; }
+        public string WikiUrl { get; set; }
+    }
+}
