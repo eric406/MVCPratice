e866d3d3d23579a4a844df7b8e13a0204b74b926
EricHsu <ericeric4062@gmail.com>
2021-11-04 11:37:57 +0800

    feature: PartialView cardlist

---
+++ MVCPratice/Controllers/PartialViewController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPratice.Models.PartialView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPratice.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        public List<Card> cards { get; } = new List<Card>
+        {
+             new Card { Name = "Elon Musk", Brief="特斯拉創辦人 伊隆·馬斯克", Photo="ElonMusk.jpg", WikiUrl="https://goo.gl/46xeXx" },
+            new Card { Name = "Mark Zuckerberg", Brief="Facebook創辦人 馬克·祖伯克", Photo="MarkZuckerberg.jpg", WikiUrl="https://goo.gl/BktGGA" },
+            new Card { Name = "Steve Jobs", Brief="蘋果創辦人 史提夫·賈伯斯", Photo="SteveJobs.jpg", WikiUrl="https://goo.gl/nAiX0y" },
+            new Card { Name = "Vader", Brief="帝國元帥  維達", Photo="Vader.jpg", WikiUrl="https://en.wikipedia.org/wiki/Darth_Vader" },
+            new Card { Name = "Darth Mual", Brief="星際大戰 達斯摩", Photo="DarthMual.jpg", WikiUrl="https://goo.gl/5obLhX"},
+            new Card { Name = "White Twilek", Brief="星際大戰 女絕地武士Twilek", Photo="WhiteTwilek.jpg", WikiUrl="https://goo.gl/reKzAu" },
+            new Card { Name = "Obiwan", Brief="星際大戰 歐比旺Obiwan", Photo="Obiwan.jpg", WikiUrl="http://bit.ly/33gxdgt" },
+            new Card { Name = "Merkel", Brief="德國總理 梅克爾", Photo="Merkel.jpg", WikiUrl="http://bit.ly/33huSlv" }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleCard()
+        {
+            return View();
+        }
+        public IActionResult CardList()
+        {
+            return View(cards);
+        }
+    }
+}
+
---
+++ MVCPratice/Models/PartialView/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPratice.Models.PartialView
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brief { get; set; }
+        public string Photo { get; set; }
+        public string WikiUrl { get; set; }
+    }
+}
